a161f2834c06525a9dc0a8d2cf0c91e6d09575f7
pilarRomero <pily_romero@yahoo.com.mx>
2023-11-14 20:21:40 -0600

    importaciones

--- segunda entrega/segundaEntrega/src/main.jsx
+++ segunda entrega/segundaEntrega/src/main.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { App } from './App'
+import { navBar } from './components/navBar/navBar'
+import {catalogo} from './components/catalogo/catalogo'
+import { productDetail } from './components/productDetail/productDetail'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
